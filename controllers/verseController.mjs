88a64c799708eda6800c1410d77838cc67a72912
iannyip <ianyipsh@gmail.com>
2023-01-21 23:57:33 +0800

    repackaged to return one controller object

--- controllers/verseController.mjs
+++ controllers/verseController.mjs
@@ -9,35 +9,42 @@ const API_URL = "https://api.esv.org/v3/passage/text/";
 const queryParam = "John+11"
 
 // 3. AXIOS CALL
-export default async function fetchPassage () {
-  try {
-    console.log(API_URL);
-    const config = {
-      method: 'get',
-      url: API_URL,
-      headers:{
-        'Authorization': 'Token ' + process.env.ESV_API_TOKEN
-      },
-      params: {
-        "q": queryParam,
-        "include-passage-references": false,
-        "include-verse-numbers": true,
-        "include-first-verse-numbers": true,
-        "include-footnotes": false,
-        "include-headings": true,
-        "include-short-copyright": false,
-        'indent-paragraphs':0
-      },
-    };
+export default function initVerseController(){
+  const fetchPassage = async () => {
+    try {
+      console.log(API_URL);
+      const config = {
+        method: 'get',
+        url: API_URL,
+        headers:{
+          'Authorization': 'Token ' + process.env.ESV_API_TOKEN
+        },
+        params: {
+          "q": queryParam,
+          "include-passage-references": false,
+          "include-verse-numbers": true,
+          "include-first-verse-numbers": true,
+          "include-footnotes": false,
+          "include-headings": true,
+          "include-short-copyright": false,
+          'indent-paragraphs':0
+        },
+      };
 
-    const response = await axios(config);
-    console.log(response.data);
-    return response.data.passages[0];
-  } catch (error){
-    console.log(error.message)
+      const response = await axios(config);
+      console.log(response.data);
+      return response.data.passages[0];
+    } catch (error){
+      console.log(error.message)
+    }
+  }
+
+  return {
+    fetchPassage
   }
 }
 
-console.log("test");
-fetchPassage();
-console.log("test2");
+
+// console.log("test");
+// fetchPassage();
+// console.log("test2");
