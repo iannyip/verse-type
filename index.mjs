1b788b2a89ec1ed26a1bb777b9788977eed7c238
iannyip <ianyipsh@gmail.com>
2023-01-22 17:50:48 +0800

    webpack for dev

--- index.mjs
+++ index.mjs
@@ -12,6 +12,26 @@ app.get('/', (req, res) => {
 
 app.get('/verse', verseController.fetchPassage)
 
+// set up webpack in dev env
+const env = process.env.NODE_ENV || "development";
+if (env === "development"){
+  const { default: webpack } = await import('webpack');
+  const {default: webpackDevMiddleware} = await import('webpack-dev-middleware');
+  const {default: webpackHotMiddleware} = await import('webpack-hot-middleware');
+  const {default: webpackConfig} = await import("./webpack_config/webpack.dev");
+
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    writeToDisk: (filePath) => /\.html$/.test(filePath),
+  }));
+  app.use(webpackHotMiddleware(compiler, {
+    log: false,
+    path: '/__webpack_hmr',
+    heartbeat: 10*1000,
+  }))
+}
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
