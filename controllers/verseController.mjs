ca74cd0a6f1616e5bbe6b6f57525323823a5749c
iannyip <ianyipsh@gmail.com>
2023-01-24 22:19:59 +0800

    linting of verseController

--- controllers/verseController.mjs
+++ controllers/verseController.mjs
@@ -1,49 +1,48 @@
 // 1. PACKAGES
 import axios from 'axios';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-
 // 2. CONSTANTS
-const API_URL = "https://api.esv.org/v3/passage/text/";
-const queryParam = "John+11"
+const API_URL = 'https://api.esv.org/v3/passage/text/';
+const queryParam = 'John+11';
 
 // 3. AXIOS CALL
-export default function initVerseController(){
+export default function initVerseController() {
   const fetchPassage = async () => {
     try {
       console.log(API_URL);
       const config = {
         method: 'get',
         url: API_URL,
-        headers:{
-          'Authorization': 'Token ' + process.env.ESV_API_TOKEN
+        headers: {
+          Authorization: `Token ${process.env.ESV_API_TOKEN}`,
         },
         params: {
-          "q": queryParam,
-          "include-passage-references": false,
-          "include-verse-numbers": true,
-          "include-first-verse-numbers": true,
-          "include-footnotes": false,
-          "include-headings": true,
-          "include-short-copyright": false,
-          'indent-paragraphs':0
+          q: queryParam,
+          'include-passage-references': false,
+          'include-verse-numbers': true,
+          'include-first-verse-numbers': true,
+          'include-footnotes': false,
+          'include-headings': true,
+          'include-short-copyright': false,
+          'indent-paragraphs': 0,
         },
       };
 
       const response = await axios(config);
       console.log(response.data);
       return response.data.passages[0];
-    } catch (error){
-      console.log(error.message)
+    } catch (error) {
+      console.log(error.message);
     }
-  }
+  };
 
   return {
-    fetchPassage
-  }
+    fetchPassage,
+  };
 }
-
 
 // console.log("test");
 // fetchPassage();
